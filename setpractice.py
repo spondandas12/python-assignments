742edeed9bb5a21067cf442c08f8c1f62739c4df
spondandas12 <spondandas2911@gmail.com>
2026-01-24 14:20:43 +0530

    assignment 2

---
+++ setpractice.py
@@ -0,0 +1,10 @@
+#assignment 2
+set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+for i in set:
+    print(i)
+set1 ={1, 2, 13, 4, 15}
+print("union:",set|set1)
+print("intersection:",set&set1)
+print("difference:",set-set1)
+inter =set.intersection(set1)
+print(inter)
