320b01c4f6b2af2bb192dec814fb01f81ec29ee8
spondandas12 <spondandas2911@gmail.com>
2026-01-24 14:21:10 +0530

    assignment 3

---
+++ tuplepractice.py
@@ -0,0 +1,10 @@
+#assignment 3
+tup_1 =("bike", "cars", "planes")
+tup_2 =(10, 2, 1)
+print(tup_1)
+print(tup_2)
+tuple =tup_1 + tup_2
+print("concatenated tuple:",tuple)
+n_tuple = ((tup_1),(tup_2))
+print("nested tuple:", n_tuple)
+print("indexing in nested tuple:", n_tuple[0][0])
