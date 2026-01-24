7ba7fbb5d07a8b424f80df2c7e5b79d66e795f5f
spondandas12 <spondandas2911@gmail.com>
2026-01-24 14:23:35 +0530

    assignment 1

---
+++ listpractice.py
@@ -0,0 +1,12 @@
+#assignment1
+list1=[34,21,47,54,6,4,2,3,1]
+list1.append(20)
+print("list after appending element:",list1)
+list1.remove(54)
+print("list after removing element:",list1)
+list1.sort()
+print("sorted list:",list1)
+list1.reverse()
+print("reversed list:",list1)
+
+
