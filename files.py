a4452dd79324afb0b178bf2dfcbb661474d7e377
spondandas12 <spondandas2911@gmail.com>
2026-03-25 15:14:09 +0530

    Update files.py

--- files.py
+++ files.py
@@ -1,24 +1,24 @@
 #create file
 f=open("shivanee.txt","w")
-#f.write("hello shivanee\n")
-#print("file created successfully!!")
+f.write("hello shivanee\n")
+print("file created successfully!!")
 f.close()
 
 #read file
 f=open("shivanee.txt")
-#print(f.read())
+print(f.read())
 f.close()
 
 #append
 f=open("shivanee.txt","a")
 f.write("how are you?")
-#print("content appended successfully!!")
+print("content appended successfully!!")
 f=open("shivanee.txt")
-#print(f.read())
+print(f.read())
 f.close()
 
 f=open("shivanee.txt","r")
-#print(f.read())
+print(f.read())
 
 
 f=open("shivanee.txt","w")
