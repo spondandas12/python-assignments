02dbb1cc1d1d5b3a6bcbf492f39ae81c456441a3
spondandas12 <spondandas2911@gmail.com>
2026-04-08 15:11:36 +0530

    Create binary.py

---
+++ binary.py
@@ -0,0 +1,15 @@
+import pickle as p
+#f=open("shivanee1.dat","wb")
+#a=input("enter:")
+#p.dump(a,f)
+#f.close()
+
+#f=open("shivanee1.dat","rb")
+#print(p.load(f))
+#f.close()
+
+#with image
+f1="WhatsApp Image 2026-03-28 at 15.47.46.jpeg"
+f2=open("shivanee1.dat","wb")
+p.dump(f1,f2)
+f1.close()
