0365b856d1618c9d3c55f1d5c58415003c2eccfb
spondandas12 <spondandas2911@gmail.com>
2026-04-08 15:19:32 +0530

    Update binary.py

--- binary.py
+++ binary.py
@@ -4,12 +4,12 @@ import pickle as p
 #p.dump(a,f)
 #f.close()
 
-#f=open("shivanee1.dat","rb")
+#f=open("Om1.dat","rb")
 #print(p.load(f))
 #f.close()
 
 #with image
 f1="WhatsApp Image 2026-03-28 at 15.47.46.jpeg"
-f2=open("shivanee1.dat","wb")
+f2=open("Om1.dat","wb")
 p.dump(f1,f2)
 f1.close()
